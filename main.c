1561008ed346246eec713d747e58d7b85c981c9d
mihail <mihail-mojsoski@outlook.com>
2021-04-06 01:01:56 +0200

    Added some code for reading from TCP

--- main.c
+++ main.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
 #include "mh/mh_tcp.h"
 #include "mh/mh_stream.h"
+const size_t copy_buffer_size = 128;
+const size_t max_request_size = 1024;
+
 void on_connect(int socket, mh_socket_address address) {
+    // Create the streams
     mh_stream_t* socket_stream = mh_socket_stream_new(socket);
-    mh_stream_t* request = mh_memory_stream_new(1024, false);
-    mh_stream_copy_to(request, socket_stream, 1024);
-    mh_stream_seek(request, 0);
+    mh_stream_t* request = mh_memory_stream_new(copy_buffer_size, false);
+
+    // Read the whole request
+    do {
+        mh_stream_copy_to(request, socket_stream, copy_buffer_size);
+        if (mh_stream_get_size(request) >= max_request_size) {
+            break;
+        }
+    } while(mh_stream_get_position(request) == mh_stream_get_size(request));
+
+
     mh_memory* request_memory = mh_memory_stream_get_memory(request);
-    printf("%s\n", (char*)request_memory->address);
+    printf("%zu %s\n", mh_stream_get_position(request), (char*)request_memory->address);
+
+    // Free the memory
     mh_stream_free(socket_stream);
     mh_stream_free(request);
 }
