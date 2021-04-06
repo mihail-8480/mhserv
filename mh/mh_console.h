70047697347b7acb6f7bb5d46f532edf1cf0bceb
mihail <mihail-mojsoski@outlook.com>
2021-04-06 19:41:46 +0200

    fixed a problem with the console

--- mh/mh_console.h
+++ mh/mh_console.h
@@ -12,7 +12,6 @@ typedef struct mh_console_static_read {
 } mh_console_static_read_t;
 
 typedef struct mh_console {
-    mh_destructor_t destructor;
     struct mh_console (*open)();
     bool opened;
     mh_stream_t* _out;
@@ -25,18 +24,20 @@ typedef struct mh_console {
     char **argv;
 } mh_console_t;
 
+
+void mh_console_output_write(const char *str);
+
+void mh_console_error_write(const char *str);
+
+char* mh_console_input_read(size_t size);
+
 mh_console_t mh_console_open(void);
-static mh_console_t mh_internal_console = { .open = mh_console_open, .opened = false };
-
-#ifdef MH_MAIN
-int mh_main(mh_console_t console);
-
-int main(int argc, char *argv[]) {
-    mh_internal_console = mh_internal_console.open();
-    mh_internal_console.argc = argc;
-    mh_internal_console.argv = argv;
-    return mh_main(mh_internal_console);
-}
-#endif
+static mh_console_t mh_console = {
+        .open = mh_console_open,
+        .opened = false,
+        .output.write = mh_console_output_write,
+        .input.read = mh_console_input_read,
+        .error.write = mh_console_error_write
+};
 
 #endif //MHSERV_MH_CONSOLE_H
