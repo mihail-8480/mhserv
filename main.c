a180639e7a1a41da11e2f48873ce754135d4280e
mihail <mihail-mojsoski@outlook.com>
2021-04-06 19:54:13 +0200

    added a 404 page

--- main.c
+++ main.c
@@ -1,13 +1,23 @@
 #include "mh/mh_tcp.h"
 #include "mh/mh_http.h"
 
-void my_request_handler(mh_stream_t *socket_stream, mh_http_request_t *request) {
-    // Write some stuff to the server
-    ECHO("HTTP/1.1 200 OK" ENDL);
+void generate_404(mh_stream_t *socket_stream, mh_http_request_t *request) {
+    // Send the headers
+    ECHO("HTTP/1.1 404 Not Found" ENDL);
     ECHO("Content-Type: text/html; charset=UTF-8" ENDL);
     ECHO("Connection: close" ENDL);
     ECHO(ENDL);
-    ECHO("<h1>Hello, World!</h1>");
+
+    // Send the content
+    ECHO("<h1>Not Found</h1>");
+    ECHO("<p>The page you have requested at ");
+    mh_stream_write(socket_stream, &request->url, request->url.size);
+    ECHO(" was not found!</p>");
+}
+
+void my_request_handler(mh_stream_t *socket_stream, mh_http_request_t *request) {
+    // Currently it only sends a 404
+    generate_404(socket_stream, request);
 }
 
 int main(void) {
