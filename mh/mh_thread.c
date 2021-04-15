beebdc742c07dc26e9e5133480b63279694dcdcc
mihail <mihail-mojsoski@outlook.com>
2021-04-15 04:26:32 +0200

    Removed windows threads because mingw-w64 can use pthreads.

--- mh/mh_thread.c
+++ mh/mh_thread.c
@@ -1,29 +1,19 @@
 #include "mh_thread.h"
-#ifndef WIN32
 #include <pthread.h>
-#else
-#include <windows.h>
-#endif
 void mh_thread_create(mh_thread_action_t action, void *args) {
     // Create the new thread and set it's arguments
-#ifndef WIN32
     pthread_attr_t attr;
     pthread_t thread;
     pthread_attr_init(&attr);
     pthread_attr_setdetachstate(&attr, 1);
     pthread_create(&thread, &attr, action, args);
     pthread_attr_destroy(&attr);
-#else
-    DWORD id;
-    CreateThread(0, 0, (LPTHREAD_START_ROUTINE)action, args, 0 , &id);
-#endif
 }
 
 void mh_thread_exit(void *ret) {
     // Exit from the thread
-#ifndef WIN32
     pthread_exit(ret);
-#else
-    ExitThread(ret);
+#ifdef WIN32
+    abort(); // to fix a warning
 #endif
 }
