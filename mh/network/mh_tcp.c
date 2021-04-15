307f9c20e4129915a089e8ff0b89f31403ac68c9
mihail <mihail-mojsoski@outlook.com>
2021-04-15 12:56:36 +0200

    removed

--- mh/network/mh_tcp.c
+++ mh/network/mh_tcp.c
@@ -118,12 +118,8 @@ void mh_tcp_start(mh_context_t *context, const mh_socket_address_t address, cons
 }
 
 unsigned short mh_tcp_address_to_string(char *host, mh_socket_address_t address, size_t size) {
-#ifndef HAIKU
     inet_ntop(address.sin_family, &address.sin_addr, host, size);
     return ntohs(address.sin_port);
-#else
-    return 0;
-#endif
 }
 
 mh_socket_address_t mh_tcp_string_to_address(const char *host, unsigned short port) {
@@ -131,8 +127,6 @@ mh_socket_address_t mh_tcp_string_to_address(const char *host, unsigned short po
             .sin_family = AF_INET,
             .sin_port = htons(port)
     };
-#ifndef HAIKU
     inet_pton(address.sin_family, host, &address.sin_addr);
-#endif
     return address;
 }
