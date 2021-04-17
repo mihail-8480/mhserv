f5a0e3468cc4ec151e3c99685b2f35576355932f
mihail <mihail-mojsoski@outlook.com>
2021-04-17 18:35:04 +0200

    added a memory leak test for windows in the tests

--- src/tests/tests.c
+++ src/tests/tests.c
@@ -1,6 +1,9 @@
 #include "tests.h"
 #include <stdio.h>
 #include <stdlib.h>
+#ifdef WIN32
+#include <crtdbg.h>
+#endif
 
 void mh_test_run(mh_test_t test, const char* name) {
     printf("Running %s", name);
@@ -21,4 +24,9 @@ int main(void) {
     MH_TEST_RUN(stream_test);
     MH_TEST_RUN(create_thread_test);
     MH_TEST_RUN(context_create_test);
+#ifdef WIN32
+    _CrtSetDbgFlag ( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
+    _CrtSetReportMode( _CRT_WARN, _CRTDBG_MODE_DEBUG );
+    _CrtDumpMemoryLeaks();
+#endif
 }
