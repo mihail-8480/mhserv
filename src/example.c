e1c820a3725693fa6ecee6dbecc9bc88c39d380a
mihail <mihail-mojsoski@outlook.com>
2021-05-02 09:49:05 +0200

    Updated the example to include the libmh.so version.

--- src/example.c
+++ src/example.c
@@ -1,5 +1,8 @@
 #include <mh_http_handler.h>
 
+static char version_string[32];
+static size_t version_len = 0;
+
 MH_UNUSED void mh_http_handle(mh_http_request_t *request) {
     // Send the headers
     MH_ECHO("HTTP/1.1 200 OK" MH_ENDL);
@@ -16,5 +19,17 @@ MH_UNUSED void mh_http_handle(mh_http_request_t *request) {
         // If it isn't - print iT wOrKs and the platform
         MH_ECHO("<h1>IT WORKS!!!</h1>");
         MH_ECHO("<p>Running on <b>" MH_PLATFORM "</b>.</p>");
+
+        // Create the version string if there isn't one already created
+        if (version_len == 0) {
+            mh_version_t version = mh_get_version();
+            sprintf(version_string, "libmh.so %d.%d.%d", version.major, version.minor, version.patch);
+            version_len = strlen(version_string);
+        }
+
+        // Print the libmh.so version
+        MH_ECHO("<p>Using: <code>");
+        mh_stream_write_reference(MH_SOCKET_STREAM, version_string, version_len);
+        MH_ECHO("</code></p>");
     }
 }
