3061444e3e4c7ca7463621c9b95f90091bbf817f
mihail <mihail-mojsoski@outlook.com>
2021-05-02 11:18:13 +0200

    Attempt to remove the submodule.

---
+++ Mh.cmake
@@ -0,0 +1,41 @@
+cmake_minimum_required(VERSION 3.0)
+
+# OS-specific stuff
+if(UNIX AND NOT (APPLE OR HAIKU))
+    add_compile_definitions(UNIX_LIKE)
+    set(UNIX_LIKE 1)
+endif(UNIX AND NOT (APPLE OR HAIKU))
+
+if (WIN32)
+    add_compile_definitions(WIN32)
+endif (WIN32)
+
+if (UNIX)
+    add_compile_definitions(UNIX)
+endif(UNIX)
+
+if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+    add_compile_definitions(LINUX)
+    set(LINUX 1)
+endif(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+
+if (HAIKU)
+    add_compile_definitions(HAIKU)
+    add_compile_definitions(_BSD_SOURCE)
+endif(HAIKU)
+
+# If you build using "Debug" enable MH_DEBUG
+if (CMAKE_BUILD_TYPE STREQUAL "Debug")
+    add_compile_definitions(MH_DEBUG)
+endif (CMAKE_BUILD_TYPE STREQUAL "Debug")
+
+# Set the common C compiler flags
+set(CMAKE_C_STANDARD 99)
+set(CMAKE_C_FLAGS "-Wall -Wextra -Wpedantic -Werror")
+set(CMAKE_C_FLAGS_DEBUG "-ggdb3")
+set(CMAKE_C_FLAGS_RELEASE "-O3")
+
+# Set the output to "bin"
+set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
+set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
