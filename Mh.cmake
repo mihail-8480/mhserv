67719f3be0e6b90a81cd3c9a970c190fa980b615
mihail <mihail-mojsoski@outlook.com>
2021-05-08 18:05:36 +0200

    Removed tests and update Mh.cmake to match the current libmh.

--- Mh.cmake
+++ Mh.cmake
@@ -1,10 +1,10 @@
 cmake_minimum_required(VERSION 3.0)
 
 # OS-specific stuff
-if(UNIX AND NOT (APPLE OR HAIKU))
+if (UNIX AND NOT (APPLE OR HAIKU))
     add_compile_definitions(UNIX_LIKE)
     set(UNIX_LIKE 1)
-endif(UNIX AND NOT (APPLE OR HAIKU))
+endif (UNIX AND NOT (APPLE OR HAIKU))
 
 if (WIN32)
     add_compile_definitions(WIN32)
@@ -12,17 +12,17 @@ endif (WIN32)
 
 if (UNIX)
     add_compile_definitions(UNIX)
-endif(UNIX)
+endif (UNIX)
 
-if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+if (CMAKE_SYSTEM_NAME STREQUAL "Linux")
     add_compile_definitions(LINUX)
     set(LINUX 1)
-endif(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+endif (CMAKE_SYSTEM_NAME STREQUAL "Linux")
 
 if (HAIKU)
     add_compile_definitions(HAIKU)
     add_compile_definitions(_BSD_SOURCE)
-endif(HAIKU)
+endif (HAIKU)
 
 # If you build using "Debug" enable MH_DEBUG
 if (CMAKE_BUILD_TYPE STREQUAL "Debug")
@@ -32,7 +32,7 @@ endif (CMAKE_BUILD_TYPE STREQUAL "Debug")
 # Set the common C compiler flags
 set(CMAKE_C_STANDARD 99)
 set(CMAKE_C_FLAGS "-Wall -Wextra -Wpedantic -Werror")
-set(CMAKE_C_FLAGS_DEBUG "-ggdb3")
+set(CMAKE_C_FLAGS_DEBUG "-ggdb3 -Og")
 set(CMAKE_C_FLAGS_RELEASE "-O3")
 
 # Set the output to "bin"
